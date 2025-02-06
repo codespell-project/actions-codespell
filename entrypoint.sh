8da81ddc4cca0e22ab83de98cea81032cbd927b2
Sergei Petrosian <spetrosi@redhat.com>
2025-02-06 09:07:30 +0100

    Add after_context, before_context, context inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,6 +52,18 @@ echo "Files or directories to check: '${INPUT_SKIP}'"
 if [ "x${INPUT_FILES}" != "x" ]; then
     command_args="${command_args} --regex ${INPUT_FILES}"
 fi
+echo "Print a number of lines of trailing context: '${INPUT_AFTER_CONTEXT}'"
+if [ "x${INPUT_AFTER_CONTEXT}" != "x" ]; then
+    command_args="${command_args} --after-context ${INPUT_AFTER_CONTEXT}"
+fi
+echo "Print a number of lines of leading context: '${INPUT_BEFORE_CONTEXT}'"
+if [ "x${INPUT_BEFORE_CONTEXT}" != "x" ]; then
+    command_args="${command_args} --before-context ${INPUT_BEFORE_CONTEXT}"
+fi
+echo "Print a number of lines of surrounding context: '${INPUT_CONTEXT}'"
+if [ "x${INPUT_CONTEXT}" != "x" ]; then
+    command_args="${command_args} --context ${INPUT_CONTEXT}"
+fi
 echo "Resulting CLI options ${command_args}"
 exec 5>&1
 res=`{ { codespell --count ${command_args} ${INPUT_PATH}; echo $? 1>&4; } 1>&5; } 4>&1`
